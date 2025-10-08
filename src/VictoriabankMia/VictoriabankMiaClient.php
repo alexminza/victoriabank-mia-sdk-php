d73d3de40f9bdd1995667b4447b2129c2092cfd2
Alexander Minza <alexander.minza@gmail.com>
2025-10-08 13:57:05 +0300

    VictoriabankMia

---
+++ src/VictoriabankMia/VictoriabankMiaClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Victoriabank\VictoriabankMia;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Command\Guzzle\DescriptionInterface;
+use GuzzleHttp\Command\Guzzle\GuzzleClient;
+use GuzzleHttp\Command\Result;
+use GuzzleHttp\Exception\BadResponseException;
+
+class VictoriabankMiaClient extends GuzzleClient
+{
+}
---
+++ src/VictoriabankMia/VictoriabankMiaDescription.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Victoriabank\VictoriabankMia;
+
+use GuzzleHttp\Command\Guzzle\Description;
+
+class VictoriabankMiaDescription extends Description
+{
+    public function __construct(array $options = [])
+    {
+    }
+}
